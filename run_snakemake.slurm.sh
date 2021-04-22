0cde5758455de9b7c3de4fb2e08c2ea3c7282185
Mallory Morgan <mallory.morgan@login2.ufhpc>
2021-04-22 10:40:25 -0400

    broad updates, sry

--- run_snakemake.slurm.sh
+++ run_snakemake.slurm.sh
@@ -7,7 +7,7 @@
 #SBATCH --cpus-per-task=1            # Number of CPU cores per task
 #SBATCH --mem=15gb                  # Total memory limit
 #SBATCH --time=192:00:00              # Time limit hrs:min:sec
-#SBATCH --output=timecourse_%j.out     # Standard output and error log
+#SBATCH --output=master_%j.out     # Standard output and error log
 #SBATCH --qos=peter
 #SBATCH --account=peter
 
@@ -15,5 +15,5 @@
 unset TMPDIR
 module load python3
 
-snakemake --configfile config.yaml --snakefile Snakefile -c 20 --jobs 10 --directory . --cluster-config hipergator.cluster.json --cluster "sbatch --qos={cluster.qos} -c {cluster.c} -n {cluster.N} --mail-type=FAIL --mail-user=mallory.morgan@ufl.edu -t {cluster.time} --mem={cluster.mem} -J "timecourse" -o timecourse_%j.out -D /blue/peter/mallory.morgan/rnastar_deseq2_MeJA_timecourse"
+snakemake --rerun-incomplete --configfile config.yaml --snakefile Snakefile -c 20 --jobs 10 --directory . --cluster-config hipergator.cluster.json --cluster "sbatch --qos={cluster.qos} -c {cluster.c} -n {cluster.N} --mail-type=FAIL --mail-user=mallory.morgan@ufl.edu -t {cluster.time} --mem={cluster.mem} -J "timecourse" -o timecourse_%j.out -D /blue/peter/mallory.morgan/rnastar_deseq2_MeJA_timecourse"
 #snakemake --configfile config/config.yaml --snakefile ./workflow/Snakefile -c 1 --jobs 1 --directory . --cluster-config ../hipergator.cluster.json --cluster "sbatch --qos={cluster.qos} -p {cluster.partition} -c {cluster.c} -n {cluster.N} --mail-type=FAIL --mail-user=d.ence@ufl.edu -t {cluster.time} --mem={cluster.mem} -J "fr1_align" -o fr1_align_%j.out -D /home/d.ence/projects/pinus_taeda_L/Fr1_project/test_pipelines/snakemake_pipelines/pipelines_for_hipergator/fr1_project_snakefiles"
